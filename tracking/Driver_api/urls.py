f2d0ce8fb7423493ff0a86be279a149cc55a4811
YousefAhmadt <yousef.ahmad.alfqeh@gmail.com>
2022-04-20 15:35:13 +0300

    api round_list student_list1

--- tracking/Driver_api/urls.py
+++ tracking/Driver_api/urls.py
@@ -6,4 +6,6 @@ from Driver_api import views
 urlpatterns = [
     path('generate-api-token/', obtain_auth_token),
     path('login/', views.driver_login),
+    path('rest/driver/round_list/', views.round_list),
+    path('rest/driver/round-students-list/<int:round_id>', views.student_list),
 ]
