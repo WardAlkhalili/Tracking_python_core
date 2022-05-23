9308e3cca73e16e83c33a42c3beb05d7c7059e44
YousefAhmadt <yousef.ahmad.alfqeh@gmail.com>
2022-05-23 13:10:04 +0300

    Update urls.py and views.py

--- tracking/Driver_api/urls.py
+++ tracking/Driver_api/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from Driver_api import views
 
-
 urlpatterns = [
     path('generate-api-token/', obtain_auth_token),
     path('login/', views.driver_login),
     path('round_list/', views.round_list),
+    path('set_round_status/', views.set_round_status),
     path('recent-notifications/', views.recent_notifications),
     path('round-students-list/<int:round_id>', views.student_list),
 ]
