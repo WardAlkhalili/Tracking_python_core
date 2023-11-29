4d9bcc0055c0cece199580ab433df25a3c1ece76
YousefAlfaqeeh <y.alfaqeeh@trackware.com>
2023-11-29 11:19:03 +0300

    Update views.py

--- Parent_api/views.py
+++ Parent_api/views.py
@@ -428,7 +428,8 @@ def parent_login(request):
             response1 = requests.request("POST", url, headers=headers, data=body)
 
             response = response1.json()
-            print(response)
+            # print(response)
+            print(response,"response--------------------------------------")
             if "error" in response:
                 result = {
                     "status": "erorrq"}
@@ -437,9 +438,9 @@ def parent_login(request):
             uid = response['result']['uid']
             company_id = response['result']['company_id']
 
-        except Exception:
+        except Exception  as error:
             print("--------------------------------------")
-            print(Exception)
+            print(error)
             result = {
                 "status": "erorr2"
                           ""}
