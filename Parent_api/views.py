02bb7f587d47f29e4e3da782fc56021bec7c63fe
YousefAlfaqeeh <y.alfaqeeh@trackware.com>
2024-05-29 09:50:01 +0300

    Update views.py

--- Parent_api/views.py
+++ Parent_api/views.py
@@ -3710,7 +3710,7 @@ def post_workSheet(request):
                             }
 
                             url = str(base_url) + "upload_worksheet"
-                            print(url)
+
                             response1 = requests.request("POST", url,headers=headers, data=body)
                             print(response1)
                             print(response1.json())
