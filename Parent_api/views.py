0d3d443ad925d13e932977ea4e4d975102763d25
YousefAlfaqeeh <y.alfaqeeh@trackware.com>
2024-10-21 12:22:23 +0300

    Update views.py

--- Parent_api/views.py
+++ Parent_api/views.py
@@ -526,12 +526,9 @@ def parent_login(request):
                         ]
                     }
                 ],
-                # "sms_system": sms_system,
-                # "tracking_system": tracking_system,
-                # "full_system": full_system,
-                "tracking_system": True,
-                "full_system": False,
-                "sms_system": False,
+                "sms_system": sms_system,
+                "tracking_system": tracking_system,
+                "full_system": full_system,
                 "uid": uid,
                 "session_id": session.get_dict()['session_id'],
                 "web_base_url": response['result']['web_base_url'],
