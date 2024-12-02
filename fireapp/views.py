ef26b8b8c6be87df0a1e8bca171b6bafa6bf4e6d
YousefAlfaqeeh <y.alfaqeeh@trackware.com>
2024-12-02 11:16:42 +0300

    Update views.py

--- fireapp/views.py
+++ fireapp/views.py
@@ -225,8 +225,8 @@ def send_message(token, body, title, data):
     else:
         try:
             with connections[school_name[0][0]].cursor() as cursor:
-                date_string = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                date = datetime.datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
+                date_string = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                date = datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
                 cursor.execute(
                     "INSERT INTO parent_message(parent_id,create_date,message,arrived_message_flg,arrived_message)VALUES (%s,%s,%s,%s,%s);",
                     [parent_id[0][0], date, body, False,
