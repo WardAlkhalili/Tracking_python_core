82d722707009842fba0822e804bc711f67680175
wardeh-alkhalili <ms.ward.alkhalili@gmail.com>
2025-03-23 14:01:24 +0300

    Update views.py

--- Parent_api/views.py
+++ Parent_api/views.py
@@ -1820,12 +1820,12 @@ def kids_hstory_new(request):
                                 information_schema = cursor.fetchall()
                                 if information_schema:
                                     cursor.execute(
-                                        "select  date,message_en,message_ar,title,title_ar,action_id,id,image_link,read_message,plan_name,school_message_id,model_school_messsage from message_student WHERE  branch_id = %s And year_id = %s  And student_id = %s AND (show_message  is null or show_message=true) AND date >= CURRENT_DATE - INTERVAL '2 months'  ORDER BY ID DESC ",
+                                        "select  date,message_en,message_ar,title,title_ar,action_id,id,image_link,read_message,plan_name,school_message_id,model_school_messsage from message_student WHERE  branch_id = %s And year_id = %s  And student_id = %s AND (show_message  is null or show_message=true) ORDER BY ID DESC ",
                                         [branch_id[0][0], branch_id[0][1], student[0]])
                                     student_mes = cursor.fetchall()
                                 else:
                                     cursor.execute(
-                                        "select  date,message_en,message_ar,title,title_ar,action_id,id,read_message,school_message_id,model_school_messsage from message_student WHERE  branch_id = %s And year_id = %s  And student_id = %s AND (show_message  is null or show_message=true) AND date >= CURRENT_DATE - INTERVAL '2 months' ORDER BY ID DESC",
+                                        "select  date,message_en,message_ar,title,title_ar,action_id,id,read_message,school_message_id,model_school_messsage from message_student WHERE  branch_id = %s And year_id = %s  And student_id = %s AND (show_message  is null or show_message=true) ORDER BY ID DESC",
                                         [branch_id[0][0], branch_id[0][1], student[0]])
                                     student_mes = cursor.fetchall()
                                 # cursor.execute(
