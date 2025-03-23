9a5c90ccfa92000a960ec9fa71dbf1305f2483a9
wardeh-alkhalili <ms.ward.alkhalili@gmail.com>
2025-03-23 20:31:35 +0300

    Update views.py

--- Parent_api/views.py
+++ Parent_api/views.py
@@ -4752,7 +4752,7 @@ def get_marks(request, student_id):
 
         # Fetch Marks Data
         cursor.execute("""
-            SELECT ml.subject_id, ml.mark_line_id, ml.mark, ss.name, mm.class_id, mm.exams, mm.semester_id 
+            SELECT mm.subject_id, ml.mark_line_id, ml.mark, ss.name, mm.class_id, mm.exams, mm.semester_id 
             FROM mark_line ml
             JOIN mark_mark mm ON ml.mark_line_id = mm.id
             JOIN school_subject ss ON mm.subject_id = ss.id
