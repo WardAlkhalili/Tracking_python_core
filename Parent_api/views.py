e5a704f4ebec7ef007818115498027584000a973
YousefAhmadt <86191065+YousefAhmadt@users.noreply.github.com>
2025-10-25 20:12:22 +0300

    Update views.py

--- Parent_api/views.py
+++ Parent_api/views.py
@@ -3536,10 +3536,15 @@ def post_attendance(request):
                         base_url = request.data.get('base_url')
                         print(school_name,student_id,start_date,end_date,type,notes,departure_time,reason,Session)
                         with connections[school_name].cursor() as cursor:
+                           
                             if "type" =="غياب":
                                 type="absence"
                             else :
                                 type="late"
+                            if reason =="مرض"
+                               reason='sick'
+                            else if reason =='حادث'
+                                 reason ="accidental"
                             attached_files = request.data.get("file")
                             body = json.dumps({"jsonrpc": "2.0",
                                                "params": {"student_id": int(student_id), "attachments": attached_files,
