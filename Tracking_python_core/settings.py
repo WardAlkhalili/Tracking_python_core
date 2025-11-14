dd0102d933fbc64a5d044ea93d846a4729d9d8a4
Yousef Ahmad <yousef@domedia.me>
2025-11-14 23:50:41 +0300

    test Wsgi

--- Tracking_python_core/settings.py
+++ Tracking_python_core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a7qjks^)2_g&32($+f8=%e3i=jogr*%$lf9prkaws-uoi+jfhu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
