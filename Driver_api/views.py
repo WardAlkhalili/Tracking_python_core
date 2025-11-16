b940507e3a575e0ab003c04fd6a81dd73bc53e17
Yousef Ahmad <yousef@domedia.me>
2025-11-16 12:47:52 +0300

    test Wsgi

--- Driver_api/views.py
+++ Driver_api/views.py
@@ -1898,7 +1898,7 @@ def students_bus_checks(request):
                                         if student_name[0][4]:
                                             parent_id.append(student_name[0][4])
                                         parent_id = list(dict.fromkeys(parent_id))
-                                    print("---------------------1832---", parent_id)
+                                    print("---------------------1832---", parent_id,"------------00000")
 
                                     if type(driver_id) is not int:
                                         for e in driver_id:
