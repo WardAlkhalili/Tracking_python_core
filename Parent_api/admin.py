5c8f9970cae82296906dbf0840caf549b26295db
YousefAhmadt <86191065+YousefAhmadt@users.noreply.github.com>
2025-11-29 17:33:30 +0300

    Update admin.py

--- Parent_api/admin.py
+++ Parent_api/admin.py
@@ -6,5 +6,5 @@ from django.contrib import admin
 
 class ManagerParentAdmin(admin.ModelAdmin):
     list_display = ('id','user_id','school_id','parent_id','db_name','token')
-
+    search_fields = ('parent_id','user_id')
 admin.site.register(ManagerParent,ManagerParentAdmin)
